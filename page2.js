fd90ccc08b1ec1d98f4b9d6ba9a2681149776fd6
leejaekwon <nowkwon97@gmail.com>
2023-03-07 13:04:12 +0900

    짝수 홀수를 통해 박스 배치 변경 함수 작성

--- page2.js
+++ page2.js
@@ -10,21 +10,29 @@ const page2 = document.getElementById('page2');
 // page2.style.alignContent = "center-around";
 
 /// 객체화 진행 중
-const page2StyleObject = ["width", "height", "backgroundColor", "display", "flexDirection", "justifyContent", "alignContent"];
-const page2StyleValue = ["100vw", "100vh", "#AADF7F", "flex", "column", "space-around", "center-around"];
+// const page2StyleObject = ["width", "height", "backgroundColor", "display", "flexDirection", "justifyContent", "alignContent"];
+// const page2StyleValue = ["100vw", "100vh", "#AADF7F", "flex", "column", "space-around", "center-around"];
 
-// let page2DomStyle = {
-//   firstArray : ["width", "height", "backgroundColor", "display", "flexDirection", "justifyContent", "alignContent"],
-//   secondArray : ["100vw", "100vh", "#AADF7F", "flex", "column", "space-around", "center-around"],
-//   element : root.children[1]
+// function page2Domstyling (firstArray, secondArray, element) {
+//   for (let i = 0; i < firstArray.length; i++) {
+//     element.style[firstArray[i]] = secondArray[i];
+//   };
 // };
+// page2Domstyling(page2StyleObject, page2StyleValue, page2);
 
-function page2Domstyling (firstArray, secondArray, element) {
-  for (let i = 0; i < firstArray.length; i++) {
-    element.style[firstArray[i]] = secondArray[i];
+// page2 style 객체
+let page2DomStyle = {
+  firstArray : ["width", "height", "backgroundColor", "display", "flexDirection", "justifyContent", "alignContent"],
+  secondArray : ["100vw", "100vh", "#AADF7F", "flex", "column", "space-around", "center-around"],
+  element : page2
+};
+// page2 style 적용 함수
+function page2DomStyling (object) {
+  for (let i = 0; i < object.firstArray.length; i++) {
+    object.element.style[object.firstArray[i]] = object.secondArray[i];
   };
 };
-page2Domstyling(page2StyleObject, page2StyleValue, page2);
+page2DomStyling(page2DomStyle);
 ///
 // // 원과 박스를 감싸는 div
 // const makeIntroduceBox1 = document.createElement('div');
@@ -59,55 +67,94 @@ page2Domstyling(page2StyleObject, page2StyleValue, page2);
 
 // 함수 두개를 쓰지말고 한 함수의 조건을 바꿔보자.
 // 조건을 걸어서 짝수 번째의 introduceBox는 원과 박스의 위치가 바뀌게 해야한다..
-function introdueBoxMaker(id, idName) {
-  const makeIntroduceBox = document.createElement('div');
-  const makeCircle = document.createElement('div');
-  const makeBox = document.createElement('div');
-  page2.appendChild(makeIntroduceBox);
-  makeIntroduceBox.setAttribute(id, idName);
-  
-  makeIntroduceBox.appendChild(makeCircle);
-  makeIntroduceBox.appendChild(makeBox);
 
-  makeIntroduceBox.style.width = "100%";
-  makeIntroduceBox.style.height = "15%"
-  makeIntroduceBox.style.display = "flex";
-  makeIntroduceBox.style.justifyContent = "space-around";
-  makeIntroduceBox.style.alignContent = "center"
+let makeIntroduceBoxArray = ["box1", "box2", "box3", "box4", "box5"];
 
-  makeCircle.style.width = "10%";
-  makeCircle.style.height = "100%";
-  makeCircle.style.backgroundColor = "#CB5F5F";
-  makeCircle.style.borderRadius = "50%"
+function introdueBoxMaker(containerElement, childElementOne, childElementTwo) {
+  for (let i = 0; i < makeIntroduceBoxArray.length; i++) {
+    // 요소 만들고 구조를 생성하는 부분
+    const makeIntroduceBox = document.createElement(containerElement);
+    const makeCircle = document.createElement(childElementOne);
+    const makeBox = document.createElement(childElementTwo);
+    page2.appendChild(makeIntroduceBox);
+    // 짝수 번째의 박스 원은 배치를 바꿔주는 조건
+    if(i%2 === 0) {
+      makeIntroduceBox.appendChild(makeCircle);
+      makeIntroduceBox.appendChild(makeBox);
+    }else {
+      makeIntroduceBox.appendChild(makeBox);
+      makeIntroduceBox.appendChild(makeCircle);
+    }
+    makeIntroduceBox.style.width = "100%";
+    makeIntroduceBox.style.height = "15%"
+    makeIntroduceBox.style.display = "flex";
+    makeIntroduceBox.style.justifyContent = "space-around";
+    makeIntroduceBox.style.alignContent = "center"
 
-  makeBox.style.width = "70%";
-  makeBox.style.height = "100%";
-  makeBox.style.backgroundColor = "#62A0DA";
-  makeBox.style.borderRadius = "20px";
+    makeCircle.style.width = "10%";
+    makeCircle.style.height = "100%";
+    makeCircle.style.backgroundColor = "#CB5F5F";
+    makeCircle.style.borderRadius = "50%"
+
+    makeBox.style.width = "70%";
+    makeBox.style.height = "100%";
+    makeBox.style.backgroundColor = "#62A0DA";
+    makeBox.style.borderRadius = "20px";
+  };
 };
-function introdueBoxMakerReverse(id, idName) {
-  const makeIntroduceBox = document.createElement('div');
-  const makeCircle = document.createElement('div');
-  const makeBox = document.createElement('div');
-  page2.appendChild(makeIntroduceBox);
-  makeIntroduceBox.setAttribute(id, idName);
-  
-  makeIntroduceBox.appendChild(makeBox)
-  makeIntroduceBox.appendChild(makeCircle);
+//   // 요소를 만들고 구조를 생성하는 부분
+//   const makeIntroduceBox = document.createElement('div');
+//   const makeCircle = document.createElement('div');
+//   const makeBox = document.createElement('div');
+//   page2.appendChild(makeIntroduceBox);
+//   // 원, 사각형을 감싸는 div의 id, name 설정
+//   makeIntroduceBox.setAttribute(id, idName);
+//   // 원, 사각형을 하나의 div에 포함시키기
+//   makeIntroduceBox.appendChild(makeCircle);
+//   makeIntroduceBox.appendChild(makeBox);
 
-  makeIntroduceBox.style.width = "100%";
-  makeIntroduceBox.style.height = "15%"
-  makeIntroduceBox.style.display = "flex";
-  makeIntroduceBox.style.justifyContent = "space-around";
-  makeIntroduceBox.style.alignContent = "center"
+//   makeIntroduceBox.style.width = "100%";
+//   makeIntroduceBox.style.height = "15%"
+//   makeIntroduceBox.style.display = "flex";
+//   makeIntroduceBox.style.justifyContent = "space-around";
+//   makeIntroduceBox.style.alignContent = "center"
 
-  makeCircle.style.width = "10%";
-  makeCircle.style.height = "100%";
-  makeCircle.style.backgroundColor = "#CB5F5F";
-  makeCircle.style.borderRadius = "50%"
+//   makeCircle.style.width = "10%";
+//   makeCircle.style.height = "100%";
+//   makeCircle.style.backgroundColor = "#CB5F5F";
+//   makeCircle.style.borderRadius = "50%"
 
-  makeBox.style.width = "70%";
-  makeBox.style.height = "100%";
-  makeBox.style.backgroundColor = "#62A0DA";
-  makeBox.style.borderRadius = "20px";
-};
+//   makeBox.style.width = "70%";
+//   makeBox.style.height = "100%";
+//   makeBox.style.backgroundColor = "#62A0DA";
+//   makeBox.style.borderRadius = "20px";
+// };
+
+// function introdueBoxMakerReverse(id, idName) {
+//   // 요소를 만들고 구조를 생성하는 부분
+//   const makeIntroduceBox = document.createElement('div');
+//   const makeCircle = document.createElement('div');
+//   const makeBox = document.createElement('div');
+//   page2.appendChild(makeIntroduceBox);
+// // 원, 사각형을 감싸는 div의 id, name 설정
+//   makeIntroduceBox.setAttribute(id, idName);
+//   // 원, 사각형을 하나의 div에 포함시키기
+//   makeIntroduceBox.appendChild(makeBox)
+//   makeIntroduceBox.appendChild(makeCircle);
+
+//   makeIntroduceBox.style.width = "100%";
+//   makeIntroduceBox.style.height = "15%"
+//   makeIntroduceBox.style.display = "flex";
+//   makeIntroduceBox.style.justifyContent = "space-around";
+//   makeIntroduceBox.style.alignContent = "center"
+
+//   makeCircle.style.width = "10%";
+//   makeCircle.style.height = "100%";
+//   makeCircle.style.backgroundColor = "#CB5F5F";
+//   makeCircle.style.borderRadius = "50%"
+
+//   makeBox.style.width = "70%";
+//   makeBox.style.height = "100%";
+//   makeBox.style.backgroundColor = "#62A0DA";
+//   makeBox.style.borderRadius = "20px";
+// };
