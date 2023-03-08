c8ae11dfa9aa00fd8a0b348fb8672742f05afb5b
jungjieun <je_eun2735@naver.com>
2023-03-08 22:42:47 +0900

    페이지2 통합하기

--- page2.js
+++ page2.js
@@ -1,24 +1,5 @@
 const root = document.getElementById('root');
 const page2 = document.getElementById('page2');
-// page2 style
-// page2.style.width = "100vw";
-// page2.style.height = "100vh";
-// page2.style.backgroundColor = "#AADF7F";
-// page2.style.display = "flex";
-// page2.style.flexDirection = "column";
-// page2.style.justifyContent = "space-around";
-// page2.style.alignContent = "center-around";
-
-/// 객체화 진행 중
-// const page2StyleObject = ["width", "height", "backgroundColor", "display", "flexDirection", "justifyContent", "alignContent"];
-// const page2StyleValue = ["100vw", "100vh", "#AADF7F", "flex", "column", "space-around", "center-around"];
-
-// function page2Domstyling (firstArray, secondArray, element) {
-//   for (let i = 0; i < firstArray.length; i++) {
-//     element.style[firstArray[i]] = secondArray[i];
-//   };
-// };
-// page2Domstyling(page2StyleObject, page2StyleValue, page2);
 
 // page2 style 객체
 let page2DomStyle = {
@@ -33,40 +14,6 @@ function page2DomStyling (object) {
   };
 };
 page2DomStyling(page2DomStyle);
-///
-// // 원과 박스를 감싸는 div
-// const makeIntroduceBox1 = document.createElement('div');
-// // 원
-// const makeCircle = document.createElement('div');
-// // 박스
-// const makeBox = document.createElement('div');
-// // page2에 원과 박스를 감싸는 div 추가
-// page2.appendChild(makeIntroduceBox1);
-// // 원과 박스를 감싸는 div 에 원, 박스 추가
-// makeIntroduceBox1.appendChild(makeCircle);
-// makeIntroduceBox1.appendChild(makeBox)
-// // 원과 박스를 감싸는 박스에 id = box1 설정
-// makeIntroduceBox1.setAttribute('id', 'box1');
-// /// makeIntroduceBox1 style 설정
-// // width, height 설정
-// makeIntroduceBox1.style.width = "100%";
-// makeIntroduceBox1.style.height = "20%";
-// // flex 설정
-// makeIntroduceBox1.style.display = "flex";
-// makeIntroduceBox1.style.justifyContent = "space-around";
-// makeIntroduceBox1.style.alignContent = "center";
-// /// makeCircle style 설정
-// makeCircle.style.width = "15%";
-// makeCircle.style.height = "100%";
-// makeCircle.style.backgroundColor = "#CB5F5F";
-// makeCircle.style.borderRadius = "50%";
-// /// makeBox style 설정
-// makeBox.style.width = "60%";
-// makeBox.style.height = "100%";
-// makeBox.style.backgroundColor = "#62A0DA"
-
-// 함수 두개를 쓰지말고 한 함수의 조건을 바꿔보자.
-// 조건을 걸어서 짝수 번째의 introduceBox는 원과 박스의 위치가 바뀌게 해야한다..
 
 let makeIntroduceBoxArray = ["box1", "box2", "box3", "box4", "box5"];
 
@@ -103,59 +50,96 @@ function introdueBoxMaker(containerElement, childElementOne, childElementTwo) {
     makeBox.style.borderRadius = "20px";
   };
 };
-//   // 요소를 만들고 구조를 생성하는 부분
-//   const makeIntroduceBox = document.createElement('div');
-//   const makeCircle = document.createElement('div');
-//   const makeBox = document.createElement('div');
-//   page2.appendChild(makeIntroduceBox);
-//   // 원, 사각형을 감싸는 div의 id, name 설정
-//   makeIntroduceBox.setAttribute(id, idName);
-//   // 원, 사각형을 하나의 div에 포함시키기
-//   makeIntroduceBox.appendChild(makeCircle);
-//   makeIntroduceBox.appendChild(makeBox);
+//----------------지은-------------------
+const myimg = document.createElement('div');
+const mytexts = document.createElement('div');
 
-//   makeIntroduceBox.style.width = "100%";
-//   makeIntroduceBox.style.height = "15%"
-//   makeIntroduceBox.style.display = "flex";
-//   makeIntroduceBox.style.justifyContent = "space-around";
-//   makeIntroduceBox.style.alignContent = "center"
+// mytexts >div 5 
+for (let i = 0; i < 5; i++) {
+const text = document.createElement('div');
+    text.innerText = 'text';
+    text.style.width = '90%';
+    text.style.backgroundColor = 'gray';
+    text.style.display = 'flex';
+    text.style.flexDirection = 'center';
+    
+    if (i === 4) {
+    text.style.height = '25%';
+    text.style.margin = '2%';
 
-//   makeCircle.style.width = "10%";
-//   makeCircle.style.height = "100%";
-//   makeCircle.style.backgroundColor = "#CB5F5F";
-//   makeCircle.style.borderRadius = "50%"
+    } else {
+    text.style.height = '15%';
+    text.style.margin = '2%';
+    }mytexts.appendChild(text);
+}
+//----setStyle  
+//for...in 으로 스타일 객체의 각 속성에 대해 요소에 스타일을 설정
+function setStyle(element, styles) {
+    for (const property in styles) {
+        element.style[property] = styles[property];
+    }
+}
 
-//   makeBox.style.width = "70%";
-//   makeBox.style.height = "100%";
-//   makeBox.style.backgroundColor = "#62A0DA";
-//   makeBox.style.borderRadius = "20px";
-// };
+//호출하기 page2
+setStyle(page2, {
+    display: 'flex',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    flexDirection: 'column',
+});
+//img
+setStyle(myimg, {
+    display: 'flex',
+    width: '200px',
+    height: '200px',
+    backgroundColor: 'white',
+    borderRadius: '100px',
+});
+//text
+setStyle(mytexts, {
+    display: 'flex',
+    width: '350px',
+    height: '600px',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    flexDirection: 'column',
+    color: 'white',
+});
+//mytexts> div
+//setStyle(mytexts.children,{
 
-// function introdueBoxMakerReverse(id, idName) {
-//   // 요소를 만들고 구조를 생성하는 부분
-//   const makeIntroduceBox = document.createElement('div');
-//   const makeCircle = document.createElement('div');
-//   const makeBox = document.createElement('div');
-//   page2.appendChild(makeIntroduceBox);
-// // 원, 사각형을 감싸는 div의 id, name 설정
-//   makeIntroduceBox.setAttribute(id, idName);
-//   // 원, 사각형을 하나의 div에 포함시키기
-//   makeIntroduceBox.appendChild(makeBox)
-//   makeIntroduceBox.appendChild(makeCircle);
+//});
 
-//   makeIntroduceBox.style.width = "100%";
-//   makeIntroduceBox.style.height = "15%"
-//   makeIntroduceBox.style.display = "flex";
-//   makeIntroduceBox.style.justifyContent = "space-around";
-//   makeIntroduceBox.style.alignContent = "center"
+//----------03/07추가 obj에서 꺼내쓰기
+const team = {
+    one:["사진","김동주","","","",""],
+    two:["사진","이재권","","","",""],
+    three:["사진","정지은","깃허브","je_eun2735@naver.com","서구","자기소개"],
+    four:["사진","최대건","","","",""],
+    five:["사진","임지성","","","",""]
+}
 
-//   makeCircle.style.width = "10%";
-//   makeCircle.style.height = "100%";
-//   makeCircle.style.backgroundColor = "#CB5F5F";
-//   makeCircle.style.borderRadius = "50%"
+let img = document.createElement('img');
+img.setAttribute('src','사진테스트.png');
+img.style.width = '100%';
+img.style.height = '100%';
+img.style.borderRadius ='100px';
 
-//   makeBox.style.width = "70%";
-//   makeBox.style.height = "100%";
-//   makeBox.style.backgroundColor = "#62A0DA";
-//   makeBox.style.borderRadius = "20px";
-// };
+console.log(mytexts.children[0]);
+mytexts.children[0].innerText = team.three[1];
+mytexts.children[1].innerText = team.three[2];
+mytexts.children[2].innerText = team.three[3];
+mytexts.children[3].innerText = team.three[4];
+mytexts.children[4].innerText = team.three[5];
+
+console.dir(mytexts);
+console.log(img);
+
+// 요소 추가
+page2.appendChild(myimg);
+page2.appendChild(mytexts);
+myimg.appendChild(img);
+
+
