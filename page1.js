51286747aab09b55c558ee74a32034012691eba7
wjkk21 <verstand821@gmail.com>
2023-03-14 17:09:09 +0900

    배경색수정됨

--- page1.js
+++ page1.js
@@ -1,47 +1,44 @@
-
-const pageone = document.getElementById("page1");
 // 페이지 id 받아옴
+const pageone = document.getElementById("page1");
 
-let header, title1, title2
+
 // 헤더 타이틀을 위한 선언
+let header, title1, title2
 
+// 태그 생성 
 header = document.createElement('header')
 title1 = document.createElement('h1')
 title2 = document.createElement('h2')
-// 태그 생성 
 
+// html에 태그 적용 
 pageone.appendChild(header)
 header.appendChild(title1)
 header.appendChild(title2)
-// html에 태그 적용 
-
-header.style.background = 'tomato'
-header.style.height = '20vh'
-header.style.padding = '1vh'
-console.dir(header)
-header.style.justifyContent = 'center'
-header.style.textAlign = 'center'
 
 //hader 
-
-title1.style.padding = '10px'
-
-title1.textContent = '세동서유'
-title1.fontWeight = ' 100px'
-
-console.dir(title1)
+header.style.background = '#C8B074';
+header.style.height = '20vh';
+header.style.padding = '1vh';
+//console.dir(header);
+header.style.justifyContent = 'center';
+header.style.textAlign = 'center';
 
 //title 1 제목 
+title1.style.padding = '10px';
+title1.textContent = '세동서유';
+title1.fontWeight = ' 100px';
 
-title2.style.padding = '10px'
-
-
-title2.textContent = '世 인간 세 霘 급할 동 敍 펼 서 流 흐를 유'
+console.dir(title1);
 
 //title 2 한자 내용  
+title2.style.padding = '10px';
+title2.textContent = '世 인간 세 霘 급할 동 敍 펼 서 流 흐를 유';
 
+// root
+let root = document.getElementById('root')
+root.style.background = '#88D4F4';
 
-
+// 
 
 
 /**-------------------------작업 구분선--------------------------- */
