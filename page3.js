dad8c4ed96212680c1907a262c29add6d944a9d0
dgchoi3904 <dgchoi3904@gmail.com>
2023-03-15 18:08:29 +0900

    page3용 모듈 작성, 클릭시 창이 늘어나는 이벤트 미적용.

---
+++ page3.js
@@ -0,0 +1,5 @@
+const projectContainer = require('./projectContainer.js');
+//const page3 = document.getElementById('page3');
+const project1 = projectContainer();
+//page3.appendChild(project1);
+console.log(project1);
